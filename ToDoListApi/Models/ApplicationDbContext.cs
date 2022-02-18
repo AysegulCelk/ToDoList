4a84e6ecb8fbd5015ce606d19ce154cf229291c0
AysegulCelk <gulsadress@gmail.com>
2022-02-18 12:53:12 +0300

    Add project files.

---
+++ ToDoListApi/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoListApi.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoItem>().HasData(
+                new ToDoItem() { Id = 1, Name = "Do the shopping" },
+                new ToDoItem() { Id = 2, Name = "clean your house" }
+                );
+        }
+    }
+}
---
+++ ToDoListApi/Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApi.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
